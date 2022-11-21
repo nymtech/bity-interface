0911b3533849b5333ae3f06ad05a4b12d02597f1
pierre <dommerc.pierre@gmail.com>
2022-11-21 15:29:48 +0100

    fix: currency rate problem

--- config.js
+++ config.js
@@ -3,23 +3,28 @@
 /* An example configuration file. */
 module.exports = {
   // OAuth client id
-  clientId: '',
+  clientId: "",
   // The URL to the Exchange API, something like https://exchange.api.bity.[io|com]
-  exchangeApiUrl: 'https://exchange.api.bity.com',
+  exchangeApiUrl: "https://exchange.api.bity.com",
   // The URL to the legacy V2 API, something like https://bity.[io|com]
-  legacyV2ApiUrl: '',
+  legacyV2ApiUrl: "",
   // The URL which users will go to when the dashboard button is clicked
-  bityDashboardUrl: '',
+  bityDashboardUrl: "",
   // The URL which the bookmark service is located at
-  bookmarksApiUrl: '',
-  restrictCurrenciesToSend: ['BTC'],
-  restrictCurrenciesToReceive: ['NYM'],
+  bookmarksApiUrl: "",
+  restrictCurrenciesToSend: ["BTC"],
+  restrictCurrenciesToReceive: ["NYM"],
+  defaultOrderParameters: {
+    inputCurrency: "BTC",
+    outputCurrency: "NYM",
+    inputAmount: "0.0002",
+  },
   // An OAuth configuration example.
   oauthConfig: {
-    authorizationUrl: 'https://connect.bity.com/oauth2/auth',
-    tokenUrl: 'https://connect.bity.com/oauth2/token',
-    clientId: '',
-    scopes: ['https://auth.bity.com/scopes/exchange.place'],
-    redirectUrl: 'https://buy.nymtech.net/'
-  }
+    authorizationUrl: "https://connect.bity.com/oauth2/auth",
+    tokenUrl: "https://connect.bity.com/oauth2/token",
+    clientId: "",
+    scopes: ["https://auth.bity.com/scopes/exchange.place"],
+    redirectUrl: "https://buy.nymtech.net/",
+  },
 };
