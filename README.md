8b242fb1821adb1ca25cb2789173d60d44af58d0
pierre <dommerc.pierre@gmail.com>
2022-11-23 13:17:10 +0100

    feat: add PORT env var

--- README.md
+++ README.md
@@ -8,8 +8,11 @@ npm i
 
 ### Run
 
+Provide env variable `PORT` on which the server will listen.
+The server reads the `.env` file.
+
 ```shell
-node server.js
+npm run start
 ```
 
-The server listens to `localhost:8080`.
+The server listens to `localhost:$PORT`.
--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 "use strict";
 
-const path = require('path');
-const fs = require('fs');
-const express = require('express');
+const path = require("path");
+const fs = require("fs");
+const express = require("express");
 
-const PORT = 8080;
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+
+if (!PORT) {
+  console.error('Env variable PORT is not set');
+  process.exit(1);
+}
 
 const app = express();
-const config = JSON.stringify(require('./config.js'));
-app.get('*/config.js', function (req, res) {
-  res.status(200).append('Content-Type', 'application/javascript').send('window.bityConfiguration = { exchangeClient: JSON.parse(`' + config + '`) };');
+const config = JSON.stringify(require("./config.js"));
+app.get("*/config.js", function (req, res) {
+  res
+    .status(200)
+    .append("Content-Type", "application/javascript")
+    .send(
+      "window.bityConfiguration = { exchangeClient: JSON.parse(`" +
+        config +
+        "`) };"
+    );
 });
-app.use(express.static('./'));
+app.use(express.static("./"));
 app.listen(PORT, function () {
-  return console.log(`Bity client ready http://localhost:${PORT}`);
+  console.log(`Bity interface ready at http://localhost:${PORT}`);
 });
